968f3cc331350f59a1c1592fea27bb196c21ce0f
Ammar-Exe <104218064+AmmarrBN@users.noreply.github.com>
2022-11-04 16:31:05 +0700

    Update module.sh

--- module.sh
+++ module.sh
@@ -10,7 +10,7 @@ pkg install nsnake &> //dev/null
 pkg install moon-buggy &> //dev/null
 pkg install ninvaders &> //dev/null
 echo "Package Already Installed"
-pip install requests colorama bs4
+pip install requests colorama bs4 rich datetime
 echo "pip Already Installed"
 sleep 3
 python ig.py
